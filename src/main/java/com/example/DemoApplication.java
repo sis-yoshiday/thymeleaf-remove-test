e7988244351ce999c0a83d8b670327e115a0915c
yukiyoshida <sis.yoshiday@gmail.com>
2017-01-11 11:44:12 +0900

    fix layout, change default th-version, apply formatter

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -11,15 +11,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 
-	@GetMapping("/")
-	public String index(Model model) {
+  @GetMapping("/")
+  public String index(Model model) {
 
-		model.addAttribute("rows", Collections.emptyList());
+    model.addAttribute("rows", Collections.emptyList());
 
-		return "index";
-	}
+    return "index";
+  }
 }
